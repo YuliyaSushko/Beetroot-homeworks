f5e8605b4a832805452be477e17514356b21117c
YuliyaSushko <yulchik.sushko1712@gmail.com>
2022-09-24 23:10:13 +0300

    add 12345

--- lesson-14-Adaptive/index.js
+++ lesson-14-Adaptive/index.js
@@ -1,16 +1,4 @@
-const burgerBtn = document.getElementById('js-burger');
-const headerNavigation = document.getElementById('js-header-navigation');
-
-burgerBtn.addEventListener('click', () => {
-  burgerBtn.classList.toggle('burger--open')
-  headerNavigation.classList.toggle('header__navigation--show')
+const nav = document.getElementById('nav');
+nav.addEventListener('click', () => {
+  nav.classList.toggle('active')  
 });
-
-const links = document.getElementsByClassName('js-link');
-
-for (const link of links) {
-  link.addEventListener('click', () => {
-    burgerBtn.classList.remove('burger--open');
-    headerNavigation.classList.remove('header__navigation--show');
-  });
-}
