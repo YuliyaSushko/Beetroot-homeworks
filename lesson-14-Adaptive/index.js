4c9aeaf9a12445278870321884c96f119ccd979f
YuliyaSushko <yulchik.sushko1712@gmail.com>
2022-10-07 00:23:58 +0300

    add local

--- lesson-14-Adaptive/index.js
+++ lesson-14-Adaptive/index.js
@@ -2,3 +2,21 @@ const nav = document.getElementById('nav');
 nav.addEventListener('click', () => {
   nav.classList.toggle('active')  
 });
+
+
+const burgerBtn = document.getElementById("nav");
+const headerNavigation = document.getElementById("js-header-navigation");
+
+burgerBtn.addEventListener("click", () => {
+  burgerBtn.classList.toggle("nav--open");
+  headerNavigation.classList.toggle("header__navigation--show");
+});
+
+const links = document.getElementsByClassName("js-link");
+
+for (const item of links) {
+  item.addEventListener("click", () => {
+    burgerBtn.classList.remove("nav--open");
+    headerNavigation.classList.remove("header__navigation--show");
+  });
+}
