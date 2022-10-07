122fd0be69557a774e05c48e94d980aa7faa04a2
YuliyaSushko <yulchik.sushko1712@gmail.com>
2022-10-08 00:45:26 +0300

    add changes

---
+++ lesson-JS-Цикли/index.js
@@ -0,0 +1,16 @@
+const answerNumber = 1;
+if (!(answerNumber % 2)) {
+  console.log ('Число /2');
+} 
+else {
+  console.log('Число !/2');
+};
+
+// Скорочений варіант
+answerNumber % 2 ? console.log ('Число /2') : console.log('Число !/2');
+
+
+
+console.log(10 % 3);
+// 10/3=3(1);
+
