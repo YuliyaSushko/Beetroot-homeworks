daa8863b9541033d2bd655068d7b4d24767526a4
YuliyaSushko <yulchik.sushko1712@gmail.com>
2022-11-05 21:29:16 +0200

    add 54698

--- lesson-JS-Події/index.js
+++ lesson-JS-Події/index.js
@@ -1,36 +1,26 @@
-// Створюю html-сторінку для відображення/редагування тексту. при відкритті сторінки текст відображається за допомогою тега div. 
-// при натисканні ctrl+E, замість div з'являється еучефкуф з тим же текстом, який тепер можна редагувати. при натискані ctrl+S, 
+// Створюю html-сторінку для відображення/редагування тексту. при відкритті сторінки текст відображається за допомогою тега div.
+// при натисканні ctrl+E, замість div з'являється еучефкуф з тим же текстом, який тепер можна редагувати. при натискані ctrl+S,
 // замість textarea з'являеться div  зі зміненим текстом. Вимкнути поведінку за замовчуванням для цих поєднань клавіш
 
-
-
 // Щоб було оптимізованіше і менше візуального коду, я окремо створюю дві константи, що приймають в себе наші елементи
-const textarea = document.getElementById('js-textarea');
-const block = document.getElementById('text-block');
+const textarea = document.getElementById('js-textarea')
+const block = document.getElementById('text-block')
 
-textarea.style.display = 'none';  //спочатку задаю textarea display none, щоб користувач її не бачив на початку
+textarea.style.display = 'none' //спочатку задаю textarea display none, щоб користувач її не бачив на початку
 
-document.addEventListener('keydown', function(click) {
-  textarea.innerHTML = block.innerHTML;
+document.addEventListener('keydown', function (event) {
+  textarea.innerHTML = block.innerHTML
 
-  if (click.code == 'KeyE' && (click.ctrlKey)) {    // при цій комбінації відпрацію код
-    click.preventDefault()
-    block.style.display = 'none';
-    textarea.style.display = 'block'; 
-  } 
-
-
-  else if (click.code == 'KeyS' && click.ctrlKey) {    // при цій комбінації відпрацію код
-    click.preventDefault()
-    block.innerHTML = textarea.value;
-    block.style.display = 'block';
-    textarea.style.display = 'none'; 
-  } else{                                     // і всіх інших випадках користувачу запропоную ввести ще раз саму потрібну комбінацію клавіш
-    alert('Спробуй ще раз');
+  if (event.code === 'KeyE' && (event.ctrlKey || event.metaKey)) {
+    // при цій комбінації відпрацію код для редагування тексту
+    event.preventDefault()
+    block.style.display = 'none'
+    textarea.style.display = 'block'
+  } else if (event.code === 'KeyS' && (event.ctrlKey || event.metaKey)) {
+    // при цій комбінації відпрацію код для зберігання вже відредагованого тексту
+    event.preventDefault()
+    block.innerHTML = textarea.value
+    block.style.display = 'block'
+    textarea.style.display = 'none'
   }
-  
-});
-
-
-
-
+})
